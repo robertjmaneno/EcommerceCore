5ae8c6407d254f3ec946ea74070e4378a2852286
Robert Maneno <robertjmaneno@gmail.com>
2024-05-09 11:20:02 +0200

    Finished crud operation on category

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -28,5 +28,70 @@ namespace Ecommerce.Controllers
            
             return View();
         }
+
+        public IActionResult Edit(int? Id)
+        {
+            if(Id == null && Id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == Id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null && Id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == Id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? Id)
+        {
+
+            
+                var category = _db.Categories.Find(Id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+              
+
+            
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Category");
+
+        }
+
+
+
     }
 }
--- Ecommerce/Models/Category.cs
+++ Ecommerce/Models/Category.cs
@@ -10,7 +10,7 @@ namespace Ecommerce.Models
 
         [Required]
         [DisplayName("Category Name")]
-        [MaxLength(20, ErrorMessage ="The maximum length must be less than 20")]
+        [MaxLength(60, ErrorMessage ="The maximum length of characters must be less than 20")]
         public required string Name { get; set; }
         [DisplayName("Display Order")]
        [Range(0, 100, ErrorMessage ="The display order must range from 0 to 100")]
